10e14ea12f4124e2341ed636dc82707acac86d5b
pixcelo <bodomu@gmail.com>
2024-02-19 04:01:57 +0900

    switch文

--- go-start/hello.go
+++ go-start/hello.go
@@ -259,4 +259,20 @@ func main() {
 	// 	}
 	// 	i++
 	// }
+
+	// switch
+	// sec := time.Now().Unix()
+	// rand.Seed(sec)
+	// i := rand.Int31n(10)
+
+	// switch i {
+	// case 0:
+	//     fmt.Print("zero...")
+	// case 1:
+	//     fmt.Print("one...")
+	// case 2:
+	//     fmt.Print("two...")
+	// }
+
+	// fmt.Println("ok")
 }
