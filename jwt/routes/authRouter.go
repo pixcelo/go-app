77333caa9d41abee821cd137eec0b6ff2f225626
pixcelo <bodomu@gmail.com>
2024-02-27 05:22:52 +0900

    add: 構成の大枠

--- jwt/routes/authRouter.go
+++ jwt/routes/authRouter.go
@@ -1,1 +1,10 @@
 package routes
+
+import (
+	controller "jwt/controllers"
+	"github.com/gin-gonic/gin"
+)
+func AuthRoutes(incoming Routes *gin.Engine) {
+	incomingRoutes.POST("user/signup", controller.Signup())
+	incomingRoutes.POST("user/login", controller.Login())
+}
