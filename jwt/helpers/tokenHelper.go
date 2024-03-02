2268108cf3a4d991329f019ae613ad51cb2a557d
pixcelo <bodomu@gmail.com>
2024-03-02 09:10:15 +0900

    mod: TokenHelper

--- jwt/helpers/tokenHelper.go
+++ jwt/helpers/tokenHelper.go
@@ -1,12 +1,16 @@
 package helper
 
 import (
+	"context"
 	"log"
 	"os"
 	"time"
 
 	"github.com/pixcelo/jwt/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type SignedDetails struct {
@@ -49,4 +53,39 @@ func GenerateAllTokens(email string, firstName string, lastname string, userType
 	}
 
 	return token, refreshToken, err
+}
+
+func UpdateAllTokens(signedToken string, signedRefreshToken string, userId string) {
+	var ctx, cancel = context.WithTimeout(context.background(), 100*time.Second)
+
+	var updateObj primitive.D
+
+	updateObj = append(updateObj, bson.M{"token", signedToke})
+	updateObj = append(updateObj, bson.M{"refresh_token", signedRefreshToken})
+
+	Updated_at, _ := time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	updateObj = append(updateObj, bson.M{"updated_at", Updated_at})
+
+	upsert := true
+	filter := bson.M{"user_id": user_id}
+	opt := options.UpdateOptions{
+		Upsert: &upsert,
+	}
+
+	_, err := userCollection.UpdateOne{
+		ctx,
+		filter,
+		bson.D{
+			{"$set", updateObj},
+		},
+		&opt,
+	}
+
+	defer cancel()
+
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+	return
 }
