5e3b94b8c226f9831d64b4763a04517c4bca952d
pixcelo <bodomu@gmail.com>
2024-03-03 20:10:39 +0900

    add: models using Gorm

---
+++ go-mvc/models/user.go
@@ -0,0 +1,29 @@
+package models
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	ID       uint   `gorm:"primaryKey"`
+	Username string `gorm:"unique"`
+	Password string
+}
+
+// HashPassword hashes the user's password before saving it to the database
+func (u *User) HashPassword(password string) error {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hashed)
+	return nil
+}
+
+// CheckPassword compares the provided password with the stored hash
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
