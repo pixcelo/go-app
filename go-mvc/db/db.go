943e53f26438cc1d1e6ee0bd185130a406279a16
pixcelo <bodomu@gmail.com>
2024-03-04 04:27:43 +0900

    database Gorm

---
+++ go-mvc/db/db.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"fmt"
+	"mvc_gorm_gin/config"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		config.DbHost, config.DbUser, config.DbPassword, config.DbName, config.DbPort)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	DB = db
+}
