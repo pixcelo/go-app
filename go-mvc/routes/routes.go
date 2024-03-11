3e96774192604120690928bdc5b7d434dc89d3b4
pixcelo <bodomu@gmail.com>
2024-03-12 07:10:45 +0900

    add: ルーティングまとめ

---
+++ go-mvc/routes/routes.go
@@ -0,0 +1,22 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pixcelo/go-mvc/controllers"
+)
+
+func SetupRoutes() *gin.Engine {
+	router := gin.Default()
+
+	// Controllers
+	homeCtrl := controllers.NewHomeController()
+	loginCtrl := controllers.NewLoginController()
+	dashboardCtrl := controllers.NewDashboardController()
+
+	// Routing
+	HomeRoutes(homeCtrl, router)
+	LoginRoutes(loginCtrl, router)
+	DashboardRoutes(dashboardCtrl, router)
+
+	return router
+}
