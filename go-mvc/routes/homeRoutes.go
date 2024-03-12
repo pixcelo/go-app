533cee09b38facafcaf441b98a5b09a850f0f2d6
pixcelo <bodomu@gmail.com>
2024-03-13 04:37:47 +0900

    fix: 不要なstruct削除

--- go-mvc/routes/homeRoutes.go
+++ go-mvc/routes/homeRoutes.go
@@ -5,8 +5,6 @@ import (
 	"github.com/pixcelo/go-mvc/controllers"
 )
 
-type HomeController struct{}
-
 func HomeRoutes(ctrl *controllers.HomeController, r *gin.Engine) {
 	r.LoadHTMLGlob("views/*/*")
 	r.GET("/", ctrl.ShowHome)
