fe8eb92d7099dc976abaf54f12ddaa9bf9412ce1
pixcelo <bodomu@gmail.com>
2024-03-16 15:21:16 +0900

    mod: oauth2設定をconfig.ymlに移動

--- go-mvc/config/oauthConfig.go
+++ go-mvc/config/oauthConfig.go
@@ -1,18 +1,47 @@
 package config
 
 import (
+	"fmt"
+
+	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/facebook"
 )
 
-func InitFacebookConfig() *oauth2.Config {
+type FacebookConfig struct {
+	Facebook Facebook `yaml:facebook`
+}
+
+type Facebook struct {
+	ClientId     string `yaml:clientId`
+	ClientSecret string `yaml:clientSecret`
+	RedirectURL  string `yaml:redirectURL`
+}
+
+func InitFacebookConfig() (*oauth2.Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath("config/")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, fmt.Errorf("Error: Reading config.yml file %s \n", err)
+	}
+
+	var config FacebookConfig
+
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshal error: %s \n", err)
+	}
+
 	var oauthConfig = &oauth2.Config{
-		ClientID:     "",
-		ClientSecret: "",
-		RedirectURL:  "http://localhost:8080/callback",
+		ClientID:     config.Facebook.ClientId,
+		ClientSecret: config.Facebook.ClientSecret,
+		RedirectURL:  config.Facebook.RedirectURL,
 		Scopes:       []string{"email"},
 		Endpoint:     facebook.Endpoint,
 	}
 
-	return oauthConfig
+	return oauthConfig, err
 }
