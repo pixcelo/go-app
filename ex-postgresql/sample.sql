41499d0c9d093201d3277b7388b029c876986cb9
pixcelo <bodomu@gmail.com>
2024-03-19 05:18:08 +0900

    rm: 不使用ファイル削除

--- ex-postgresql/sample.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE department
-(
-    department_code character varying(10) NOT NULL,
-	department_name character varying(100),
-    CONSTRAINT pk_department PRIMARY KEY (department_code)
-);
